2046fc0c1f0855d9863aa4ec37efdc52502a99d8
Henry Dan <baotd789@gmail.com>
2021-05-18 02:52:47 -0700

    Editing scrollbar, and state handlers

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,7 @@
 import {MyToolkit} from './mytoolkit.js';
 
 // Implement a MyToolkit Button
-var btn = new MyToolkit.Button;
+var btn = new MyToolkit.Button("Test Me");
     btn.setId("btn1");
     btn.move(20,20);
 	//event Handler for detecting clicks
@@ -12,12 +12,14 @@ var btn = new MyToolkit.Button;
     })
 	//event Handler for state Changes
 	btn.stateChanged(function(event){
-		console.log(event)
+		console.log("Button: " + event)
 	})
 
-var btn2 = new MyToolkit.Button;
+var btn2 = new MyToolkit.Button("Decrease Progress");
+btn2.setSize(140,30)
 btn2.setId("btn2");
 btn2.move(20,200);
+
 //event Handler for detecting clicks
 btn2.onclick(function(event){
 	console.log(event)
@@ -29,15 +31,14 @@ btn2.stateChanged(function(event){
 	console.log(event)
 })
 
-var cb = new MyToolkit.CheckBox;
+var cb = new MyToolkit.CheckBox("Checkbox 1");
 	cb.setId("cb1");
 	cb.move(20,65);
 	cb.onclick(function(event){
-        console.log(event)
         console.log(event.target)
     })
 	cb.stateChanged(function(event){
-		console.log(event)
+		console.log("Checkbox: " + event)
 	})
 
 let opt = new Array(
@@ -54,11 +55,28 @@ var tb = new MyToolkit.TextBox;
 
 var sb = new MyToolkit.ScrollBar(250);
 	sb.setId("sb1");
-	sb.move(340, 70);
+	sb.move(340, 65);
 	sb.onclick(function(event){
         console.log(event.target)
 		sb.increase();
     })
+	sb.stateChanged(function(event){
+		console.log("Scroll bar: " + event)
+	})
+
+var btn3 = new MyToolkit.Button("Get Scroll Nav Position");
+btn3.setSize(170,30)
+btn3.setId("btn2");
+btn3.move(20,250);
+
+//event Handler for detecting clicks
+btn3.onclick(function(event){
+	sb.getThumbPos();
+})
 
 var pb = new MyToolkit.ProgressBar(360);
 	pb.move(20, 350);
+	pb.stateChanged(function(event){
+		console.log(event)
+	})
+
